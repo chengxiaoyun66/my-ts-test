f4ca08be00cdc7f9001b0e83645e85d221d93c12
chengxiaoyun66 <1241960176@qq.com>
2026-04-30 16:47:41 +0800

    添加登录功能的E2E测试

--- e2e/example.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('example', async ({ page }) => {
-    await page.goto('https://www.baidu.com');
-    await expect(page).toHaveTitle('百度一下，你就知道');
-});
