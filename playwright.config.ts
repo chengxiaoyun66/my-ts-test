de57c8cd59c9cfbadd013e45243b1c38a41338f3
chengxiaoyun66 <1241960176@qq.com>
2026-04-30 16:24:49 +0800

    fix: set headless true for CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,7 +4,8 @@ export default defineConfig({
   testDir: './e2e',
   timeout: 30000,
   use: {
-    headless: false,
+    headless: true,
+    // headless: process.env.CI ? true : false,
     viewport: { width: 1280, height: 720 },
     // slowMo: '1000',
   },
